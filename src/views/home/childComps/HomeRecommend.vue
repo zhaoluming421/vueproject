fdec10b26f84ba1415aa11d3cced76a717ed8ee4
zhaolm <zlm_email@163.com>
2019-12-26 14:31:53 +0800

    实现首页中导航栏、轮播图的功能

---
+++ src/views/home/childComps/HomeRecommend.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item, index) in recommends" :key="index" class="recommend_item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+      };
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+  };
+</script>
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 10px 0 20px;
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommend_item {
+    flex: 1;
+  }
+
+  .recommend_item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,39 @@
+<template>
+  <swiper>
+    <swiper-item v-for="(item, index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    components: {
+      Swiper,
+      SwiperItem
+    },
+    props: {
+      banners: {
+        tyep: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+      };
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+  };
+</script>
+<style scoped>
+</style>
